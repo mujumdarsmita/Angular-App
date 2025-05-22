0611ecf6c17a07dc207d77f428bef233a390a30a
locobuzz-smita-mujumdar <104754921+locobuzz-smita-mujumdar@users.noreply.github.com>
2025-05-22 19:49:35 +0530

    Toggle changes

--- src/app/my-info/my-info.component.ts
+++ src/app/my-info/my-info.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-info',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-info.component.html',
   styleUrl: './my-info.component.css'
 })
 export class MyInfoComponent {
+bioVisible = true;
 
+  bio = "Hello! I'm Smita Mujumdar, a software developer passionate about Angular and .Net technologies.";
+
+  skills: string[] = ['Angular', 'TypeScript', '.NET Core', 'C#', 'SQL Server'];
+
+  toggleBio(): void {
+    this.bioVisible = !this.bioVisible;
+  }
 }
